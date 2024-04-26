6905c0545efd41a246c4601e4d7dbc4949d02490
Shayne Boyer <spboyer@live.com>
2024-04-26 13:47:42 -0400

    minor fix for format

--- src/eShop.AppHost/Program.cs
+++ src/eShop.AppHost/Program.cs
@@ -78,7 +78,7 @@ var webApp = builder.AddProject<Projects.WebApp>("webapp", launchProfileName)
     .WithReference(appInsights);
 
 // set to true if you want to use OpenAI
-bool useOpenAI = Convert.ToBoolean(Environment.GetEnvironmentVariable("AI__USE_OPENAI"));
+bool useOpenAI = Convert.ToBoolean(builder.Configuration["AI__USE__OPENAI"]);
 if (useOpenAI)
 {
     const string openAIName = "openai";
